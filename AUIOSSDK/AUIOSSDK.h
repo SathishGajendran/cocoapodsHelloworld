214c6626c48a8ef948d78ad8ea5617cad72940c0
Sathish G <sathish.gajendran@gmail.com>
2017-10-05 12:34:08 +0530

    hello world example

---
+++ AUIOSSDK/AUIOSSDK.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AUIOSSDK.
+FOUNDATION_EXPORT double AUIOSSDKVersionNumber;
+
+//! Project version string for AUIOSSDK.
+FOUNDATION_EXPORT const unsigned char AUIOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AUIOSSDK/PublicHeader.h>
+
+
---
+++ AUIOSSDK/main.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+
+
+public class AU {
+    
+    public var testVar: String;
+    
+    public init(){
+        self.testVar = "value";
+    }
+}
