3c80a2a51023f36ab1da9f7970936791afce2d06
Sathish G <sathish.gajendran@gmail.com>
2017-10-09 13:21:33 +0530

    changes

--- AUIOSSDK/main.swift
+++ AUIOSSDK/main.swift
@@ -6,39 +6,92 @@ import Alamofire
 
 public class AU {
     
-    public var testVar: String;
+    public var trackerId: String = "";
+    public var originatingSystemCode: String = "";
     
-    public init(){
-        self.testVar = "value";
+    public init(_ config: NSDictionary){
+        self.trackerId = config.value(forKey: "trackerId") as! String
+        self.originatingSystemCode = config.value(forKey: "originatingSystemCode") as! String
     }
     
-    public func sendEvent() {
+    public func sendEvent(_ data: NSDictionary) {
+        
+//        print(data);
+//        print(self.trackerId)
+        
+        let payload = data.value(forKey: "payload")! as! NSDictionary;
+        
         let headers: HTTPHeaders = [
-            "PETRACKER-TRACKING-ID": "PE-APP-1847-2"
+            "PETRACKER-TRACKING-ID": self.trackerId
         ]
+        
+//        print(data.value(forKey: "actionType")!);
+//        print(payload.value(forKey: "url")!)
+        
         
         let parameters: Parameters = [
             "data":[
-                "originatingSystemCode":"petracker",
+                "originatingSystemCode":self.originatingSystemCode,
                 "events":[
-                [
-                "messageTypeCode":"pageview",
-                "messageVersion":"v4",
-                "actionType":"create",
-                "namespace":"gsetracker",
-                "payload":[
-                "url":"ASD34",
-                "userID":"anonymous",
-                "useragent":"Mozilla%2F5.0%20(Windows%20NT%206.1%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F61.0.3163.100%20Safari%2F537.36"
-                ]
-                ]
+                            [
+                                "messageTypeCode":data.value(forKey: "messageTypeCode")!,
+                                "messageVersion":data.value(forKey: "messageVersion")!,
+                                "actionType":data.value(forKey: "actionType")!,
+                                "namespace":data.value(forKey: "namespace")!,
+//                                "payload":[
+//                                    "url":payload.value(forKey: "url")!,
+//                                    "userID":payload.value(forKey: "userID")!,
+//                                    "useragent":payload.value(forKey: "useragent")!
+//                                ]
+                                "payload":payload
+                            ]
+                        ]
+            ]
+        ];
+        
+//        print(parameters);
+        
+        Alamofire.request("https://testapi.english.com/autobahn/collect/events", method: .post, parameters: parameters, headers:headers).responseJSON { response in
+//            let statusCode = response.response?.statusCode;
+            print(response)
+        }
+        
+    }
+    
+    public func sendActivity(_ data: NSDictionary) {
+        
+        //        print(data);
+        //        print(self.trackerId)
+        
+        let payload = data.value(forKey: "payload")! as! NSDictionary;
+        
+        let headers: HTTPHeaders = [
+            "PETRACKER-TRACKING-ID": self.trackerId
+        ]
+        
+        //        print(data.value(forKey: "actionType")!);
+        //        print(payload.value(forKey: "url")!)
+        
+        
+        let parameters: Parameters = [
+            "data":[
+                "originatingSystemCode":self.originatingSystemCode,
+                "activities":[
+                    [
+                        "messageTypeCode":data.value(forKey: "messageTypeCode")!,
+                        "messageVersion":data.value(forKey: "messageVersion")!,
+                        "namespace":data.value(forKey: "namespace")!,
+                        "payload":payload
+                    ]
                 ]
             ]
         ];
         
-        Alamofire.request("https://testapi.english.com/autobahn/collect/events", method: .post, parameters: parameters, headers:headers).responseJSON { response in
+//        print(parameters);
+        
+        Alamofire.request("https://testapi.english.com/autobahn/collect/activities", method: .post, parameters: parameters, headers:headers).responseJSON { response in
+            //            let statusCode = response.response?.statusCode;
             print(response)
-//            return response
         }
         
     }
